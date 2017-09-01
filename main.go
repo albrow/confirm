c892fbb56d1294aae4ecfb38c0dbc49d7623f894
Alex Browne <stephenalexbrowne@gmail.com>
2017-09-01 19:38:08 -0400

    Initial implementation

---
+++ main.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"bufio"
+	"flag"
+	"os"
+	"strings"
+)
+
+const usage = `Confirm is a tiny but useful utility for confirming actions.
+
+Usage:
+
+    confirm [flags] message
+
+'message' is a message that will be printed out to the user.
+
+After the message has been printed out, confirm will wait to
+receive either 'yes' or 'y' (case-insensitive) before
+continuing. You can enter 'no' or 'n' (case-insensitive) to
+cancel the action and return a non-zero status code.
+
+The fags are:
+
+    --help
+        Print this message.
+
+`
+
+// maxAttempts is the maximum number of times to receive user input
+// before giving up. If a user enters an invalid input maxAttempts
+// times, we will exit with a non-zero status code.
+const maxAttempts = 3
+
+func main() {
+	var help bool
+	flag.BoolVar(&help, "help", false, "Print usage information.")
+	flag.Parse()
+
+	if help {
+		print(usage)
+		os.Exit(0)
+	}
+
+	if len(flag.Args()) == 0 {
+		print("Confirm requires exactly one argument, the message to be printed out.\n")
+		os.Exit(1)
+	}
+
+	message := flag.Arg(0)
+	if !strings.HasSuffix(message, "\n") {
+		message += "\n"
+	}
+	print(message)
+
+	attempts := 0
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		input := strings.ToLower(scanner.Text())
+		switch input {
+		case "y", "yes":
+			os.Exit(0)
+		case "n", "no":
+			print("Action cancelled by user.\n")
+			os.Exit(1)
+		default:
+			attempts++
+			if attempts >= maxAttempts {
+				print("Could not confirm action: Too many attempts with invalid input.\n")
+				os.Exit(1)
+			}
+			print("Unexpected input. Please try again.\n")
+			print(message)
+		}
+	}
+	print("Confirm could not read user input.\n")
+	os.Exit(1)
+}
